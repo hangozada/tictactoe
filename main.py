507cc3932b2731205001e1d342bfa6ba4ec0743a
hango <hango@Farida-MacBook-Pro.local>
2025-08-29 15:36:41 +0300

    farfoora

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def main():
     print(request.remote_addr)
     return render_template("battleship.html.j2", **data)
 
+@app.route("/farida")
+def main():
+    data = {}
+    return render_template("farida.html", **data)
+
 @app.route("/move", methods=['POST'])
 def move():
     receivedData = request.get_json(force=True)
